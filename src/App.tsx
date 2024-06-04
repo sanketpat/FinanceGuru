d60072c1dc3970eec6028bb55a958bd1c5da1665
sanketpatil <sanket.patil30@gmail.com>
2024-06-04 16:55:33 -0400

    Add dashboard support

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from "./theme"
 import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "@/scenes/navbar"
+import Dashboard from "@/scenes/dashboard"
 
 function App() {
   const theme = useMemo(()=> createTheme(themeSettings), [])
@@ -11,10 +12,10 @@ function App() {
     <BrowserRouter>
     <ThemeProvider theme={theme}>
         <CssBaseline/>
-        <Box width="100%" height="100%" padding="1rem, 2rem, 4rem, 2rem" >
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem" >
             <Navbar />
             <Routes>
-              <Route path="/" element={<div> dashboard page</div>}/>
+              <Route path="/" element={<Dashboard/>}/>
               <Route path="/predictions" element={<div>predictions page</div>}/>
             </Routes>
         </Box>
