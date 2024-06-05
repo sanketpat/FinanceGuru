ea993a2ee48018e05bf3f038f34b472c7f5ad518
sanketpatil <sanket.patil30@gmail.com>
2024-06-04 23:24:09 -0400

    update node server files

--- server/index.js
+++ server/index.js
@@ -1,39 +1,40 @@
-import express from "express"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import cors from "cors"
-import helmet from "helmet"
-import morgan from "morgan"
-import dotenv from "dotenv"
-import kpiRoutes from "./routes/kpi.js"
-import KPI from "./model/KPI.js"
-import {kpis} from "./data/data.js"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
 
-
-/*CONFIGURATIONS*/
-
-dotenv.config()
+/* CONFIGURATIONS */
+dotenv.config();
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
-/*ROUTES */
+/* ROUTES */
 app.use("/kpi", kpiRoutes);
 
-/*MONGOOSE SETUP*/
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-}).then(async()=>{
-    app.listen(PORT, () => console.log(`Server Port:${PORT}`))
-    /* Add data one time
-    await mongoose.connection.db.dropDatabase();
-    KPI.insertMany(kpis);
-    */
-}).catch((error)=> console.log(`${error} did not connect`))
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -1,14 +1,17 @@
-import express from "express"
+import express from "express";
 import KPI from "../model/KPI.js";
+
 const router = express.Router();
 
-router.get("/kpis", async(req, res)=> {
-    try{
-        const kpis = await KPI.find();
-        res.status(200).json(kpis);
-    }catch(error){
-        res.status(404).json({message:error.message})
-    }
-})
+router.get("/kpis", async (req, res) => {
+  try {
+    console.log("in /kpis"); // Log statement for debugging
+    const kpis = await KPI.find();
+    console.log("Fetched KPIs:", kpis); // Log the fetched data
+    res.status(200).json(kpis);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 export default router;
